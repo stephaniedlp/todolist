ba6f6e0c51ecd27fe773a8ca360ce7ad8cf1589e
stephaniedlp <steffy_dlp@hotmail.com>
2023-11-03 09:43:26 -0700

    todo

--- src/script.js
+++ src/script.js
@@ -1,224 +1,154 @@
-// // Variables para acceder a elementos HTML
-// const taskForm = document.getElementById('task-form');
-// const taskList = document.getElementById('task-list');
 
-// // Event listener para el formulario de tareas
-// taskForm.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     // Obtener valores del formulario
-//     const title = document.getElementById('task-title').value;
-//     const description = document.getElementById('task-description').value;
-//     const dueDate = document.getElementById('task-due-date').value;
-//     const priority = document.getElementById('task-priority').value;
-//     const labels = document.getElementById('task-labels').value.split(',');
+// Select DOM elements
+const form = document.getElementById("form");
+const title = document.getElementById("textInput").value;
+const dueDate = document.getElementById("dateInput").value;
+const description = document.getElementById("taskdescription").value;
+const priority = document.getElementById("taskpriority").value;
+const label = document.getElementById("labelInput").value;
+const msg = document.getElementById("msg");
+const tasks = document.getElementById("tasks");
+const add = document.getElementById("add");
 
-//     // Validar datos
-//     if (title.trim() === '') {
-//         alert('El título es obligatorio');
-//         return;
-//     }
 
-//     // Crear objeto de tarea
-//     const task = {
-//         title,
-//         description,
-//         dueDate,
-//         priority,
-//         labels
-//     };
+// function displayTasks(tasks){
+//   cleartask();
 
-//     // Agregar la tarea a la lista
-//     displayTask(task);
-
-//     // Limpiar el formulario
-//     taskForm.reset();
-// });
-
-// // Función para mostrar una tarea en la lista
-// function displayTask(task) {
-//     const li = document.createElement('li');
-    // li.innerHTML = `
-    //     <h3>${task.title}</h3>
-    //     <p>${task.description}</p>
-    //     <p><strong>Fecha de Vencimiento:</strong> ${task.dueDate}</p>
-    //     <p><strong>Prioridad:</strong> ${task.priority}</p>
-    //     <p><strong>Etiquetas:</strong> ${task.labels.join(', ')}</p>
-    //     <button class="delete-button">Eliminar</button>
-    // `;
-//     taskList.appendChild(li);
-
-//     // Event listener para eliminar la tarea
-//     const deleteButton = li.querySelector('.delete-button');
-//     deleteButton.addEventListener('click', function () {
-//         taskList.removeChild(li);
-//     });
 // }
-
-
-
-// function getVideoGamesData() {
-//     fetchAPI(`${apiURL}/task`, "GET")
-//     .then((data) => {
-//       const gameList = mapAPIToGameDescriptors(data);
-//       displayGameOptions(gameList);
-//     });
-//   }
-  
-//   function getSalesData(videoGames, customerName, salesman, saleDate) {
-   
-//     const url = buildGetSalesDataUrl( videoGames,  customerName,  salesman,  saleDate );
-  
-//     fetchAPI(url, "GET")
-//     .then((data) => {
-//       const salesList = mapAPIToTasks(data);
-//       displayGamesView(salesList);
-//     });
-//   }
-  
-//   function createSale(venta) {
-//     fetchAPI(`${apiURL}/venta`, "POST", venta).then((venta) => {
-//       closeAddSaleModal();
-//       resetSales();
-//       window.alert(`Venta ${venta.id} creada correctamente.`);
-//     });
-//   }
-  
-//   function deleteSale(ventaId) {
-//     const confirm = window.confirm(
-//       `¿Estás seguro de que deseas eliminar la venta ${ventaId}?`
-//     );
-  
-//     if (confirm) {
-//       fetchAPI(`${apiURL}/venta/${ventaId}`, "DELETE").then(() => {
-//         resetSales();
-//         window.alert("Venta eliminada.");
-//       });
-//     }
-//   }
-  
-//   // Funcion que genera la url para consultar ventas con filtros.
-//   function  buildGetSalesDataUrl( videoGames, customerName, salesman, saleDate) {
-//     // Tecnica de string dinamico: se aconseja cuando tenemos una cantidad limitada de parámetros y
-//     //    cierto control de los tipos de parametros (id, fechas).
-//     // const url = `${apiURL}/sales?realEstate=${realEstate}&customerName=${customerName}&salesman=${salesman}&saleDate=${saleDate}`;
-  
-//     // URL y URLSearchParams: simplifican la construcción de URLs dinámicas y complejas,
-//     //    facilitan la gestión de múltiples parámetros y textos dinámicos al encargarse de
-//     //    la codificación y decodificación de caracteres especiales, lo que evita problemas
-//     //    comunes relacionados con espacios y caracteres no válidos.
-//     const url = new URL(`${apiURL}/venta`);
-  
-//     if (videoGames) {
-//       url.searchParams.append("videoGames", videoGames);
-//     }
-  
-//     if (customerName) {
-//       url.searchParams.append("customerName", customerName);
-//     }
-  
-//     if (salesman) {
-//       url.searchParams.append("salesman", salesman);
-//     }
-  
-//     if (saleDate) {
-//       url.searchParams.append("saleDate", saleDate);
-//     }
-  
-//     return url;
-//   }
-
-
-let form = document.getElementById("form");
-let textInput = document.getElementById("textInput");
-let dateInput = document.getElementById("dateInput");
-let textarea = document.getElementById("textarea");
-let msg = document.getElementById("msg");
-let tasks = document.getElementById("tasks");
-let add = document.getElementById("add");
-
+// Add form submit event listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   formValidation();
 });
 
-let formValidation = () => {
+const formValidation = () => {
   if (textInput.value === "") {
-    console.log("failure");
     msg.innerHTML = "Task cannot be blank";
   } else {
-    console.log("success");
     msg.innerHTML = "";
     acceptData();
-    add.setAttribute("data-bs-dismiss", "modal");
-    add.click();
-
-    (() => {
-      add.setAttribute("data-bs-dismiss", "");
-    })();
   }
 };
 
-let data = [{}];
+// Accept data and send it to the API
+const acceptData = () => {
+  const taskData = {
+    title: textInput.value,
+    dueDate: dateInput.value,
+    description: taskdescription.value,
+    priority: taskpriority.value,
+    label: labelInput.value,
+  };
 
-let acceptData = () => {
-  data.push({
-    text: textInput.value,
-    date: dateInput.value,
-    description: textarea.value,
-  });
-
-  localStorage.setItem("data", JSON.stringify(data));
-
-  console.log(data);
-  createTasks();
+  // Send data to the server
+  fetch(`${apiURL}/lista`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Task creation failed');
+      }
+    })
+    .then(() => {
+      displayTasks(); // Refresh the task list
+      add.setAttribute("data-bs-dismiss", "modal");
+      add.click();
+    })
+    .catch(error => {
+      console.error('Error creating task:', error);
+    });
 };
 
-let createTasks = () => {
+// C// Function to create tasks
+const createTasks = () => {
   tasks.innerHTML = "";
-  data.map((x, y) => {
-    return (tasks.innerHTML += `
-    <div id=${y}>
-          <span class="fw-bold">${x.text}</span>
-          <span class="small text-secondary">${x.date}</span>
-          <p>${x.description}</p>
-  
-          <span class="options">
-            <button type="submit" id="btn-edit" class="btn ">Edit</button>
-            <button type="submit" id="btn-delete" class="btn">Delete</button>
-          </span>
-        </div>
-    `);
+
+  // Fetch tasks from the server
+  fetch(`${apiURL}/lista`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Task data fetch failed');
+      }
+    })
+    .then(data => {
+      displayTasks(data); // Call the displayTasks function with the retrieved data
+    })
+    .catch(error => {
+      console.error('Error fetching task data:', error);
+    });
+};
+
+// Function to display tasks
+const displayTasks = (data) => {
+  data.forEach((task, index) => {
+    tasks.innerHTML += `
+    <div class="taskslist">
+    <div class="headertasklist">
+      <span class="fw-bold">${task.title}</span>
+      <span class="priority">${task.priority}</span>
+    </div>
+    <span class="taskdate"> <p>${(task.dueDate)}</p></span>
+    <span class="tasklabel">${task.label}</span>
+    <span class="smaller text-secondary"><p>${task.description}</p></span>
+
+    <span class="options">
+      <button type="submit" id="btn-edit" class="btn-edit">Edit</button>
+      <button type="submit" id="btn-delete"  data-sale-id="${task.id}" class="btn-delete">Delete</button>
+    </span>
+  </div>
+    `;
   });
-
-  resetForm();
-};
-
-let deleteTask = (e) => {
-  e.parentElement.parentElement.remove();
-  data.splice(e.parentElement.parentElement.id, 1);
-  localStorage.setItem("data", JSON.stringify(data));
-  console.log(data);
   
+  initDeleteButtonHandler();
 };
 
-let editTask = (e) => {
-  let selectedTask = e.parentElement.parentElement;
+// Initialize the task list
+document.addEventListener("DOMContentLoaded", function() {
+  createTasks(); // Call createTasks when the DOM is loaded
+});
 
+  
+// Delete a task
+function deleteTask(listaId) {
+  const confirm = window.confirm(
+    `¿Estás seguro de que deseas eliminar la venta ${listaId}?`
+  );
+
+  if (confirm) {
+    fetchAPI(`${apiURL}/lista/${listaId}`, "DELETE").then(() => {
+      window.alert("Venta eliminada.");
+      
+    });
+  }
+  
+  window.location.reload();
+}
+
+function initDeleteButtonHandler() {
+  document.querySelectorAll(".btn-delete").forEach((button) => {
+    button.addEventListener("click", () => {
+      const listaId = button.getAttribute("data-sale-id"); // Obtenemos el ID de la venta
+      deleteTask(listaId); // Llamamos a la función para eleminar la venta
+    });
+  });
+}
+// Edit a task
+const editTask = (index) => {
+  const selectedTask = tasks.children[index];
   textInput.value = selectedTask.children[0].innerHTML;
-  dateInput.value = selectedTask.children[1].innerHTML;
+  description.value = selectedTask.children[1].innerHTML;
   textarea.value = selectedTask.children[2].innerHTML;
-
-  deleteTask(e);
+  priority.value=selectedTask.children[3].innerHTML;
+  label.value=selectedTask.children[4].innerHTML;
+  // Delete the task locally
+  deleteTask(index);
 };
 
-let resetForm = () => {
-  textInput.value = "";
-  dateInput.value = "";
-  textarea.value = "";
-};
-
-(() => {
-  data = JSON.parse(localStorage.getItem("data")) || []
-  console.log(data);
-  createTasks();
-})();
---
+++ src/utils.js
@@ -0,0 +1,30 @@
+
+  // // Formatea un número con dos decimales.
+  // function formatDecimal(number) {
+  //   if (typeof number !== 'number') {
+  //     throw new Error('El valor proporcionado no es un número.');
+  //   }
+  
+  //   return number.toFixed(2);
+  // }
+  
+  // // Formatea un número con dos decimales y lo etiqueta como metros cuadrados (m²).
+
+  
+  // // Formatea un objeto de tipo Date a un string con formato dd/mm/aaaa.
+  // function formatDate(date) {
+  //   if (!(dueDate instanceof Date)) {
+  //     throw new Error('El valor proporcionado no es una instancia de Date.');
+  //   }
+  
+  //   const day = date.getUTCDate();
+  //   const month = date.getUTCMonth() + 1; // Sumamos 1 ya que los meses son base 0.
+  //   const year = date.getUTCFullYear();
+  
+  //   // Aseguramos que los componentes de la fecha tengan al menos dos dígitos.
+  //   const formattedDay = String(day).padStart(2, '0');
+  //   const formattedMonth = String(month).padStart(2, '0');
+  
+  //   return `${formattedDay}/${formattedMonth}/${year}`;
+  // }
+  
